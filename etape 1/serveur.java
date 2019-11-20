8b1cfbe840a47e2f01b9ce1639bf094ef740710e
alexisarekion <55801978+alexisarekion@users.noreply.github.com>
2019-11-20 12:27:15 -0400

    etape 1

---
+++ etape 1/serveur.java
@@ -0,0 +1,45 @@
+package tenta4;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class serveur {
+	public static void main(String[] args) throws Exception {
+		
+		
+		 
+	 
+	    
+	    try{
+	      ServerSocket server=new ServerSocket(5000);
+	     // ecoute=new ServerSocket(PORT);
+			 System.out.println("Serveur initialisé");
+			 
+	      Socket serverClient=server.accept();
+	      DataInputStream inStream=new DataInputStream(serverClient.getInputStream());
+	      DataOutputStream outStream=new DataOutputStream(serverClient.getOutputStream());
+	      BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+	      String clientMessage="", serverMessage="";
+	      while(!clientMessage.equals("quit")){
+	        clientMessage=inStream.readUTF();
+	        System.out.println("From Client: "+clientMessage);
+	        serverMessage=reader.readLine();
+	        outStream.writeUTF(serverMessage);
+	        outStream.flush();
+	      }
+	      inStream.close();
+	      outStream.close();
+	      serverClient.close();
+	      server.close();
+	    }catch(Exception e){
+	      System.out.println(e);
+	    }
+	  }
+
+}
